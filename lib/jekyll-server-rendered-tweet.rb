c8bb81a2ea15a446f6e99238e4cd526b31181a7f
Max Chadwick <mpchadwick@gmail.com>
2020-05-11 21:53:17 -0400

    Make rubocop more happy

--- lib/jekyll-server-rendered-tweet.rb
+++ lib/jekyll-server-rendered-tweet.rb
@@ -2,9 +2,7 @@ require 'jekyll'
 require 'twitter'
 
 module Jekyll
-
   class ServerRenderedTweet < Liquid::Tag
-
     attr_writer :client
 
     def initialize(tag_name, text, tokens)
@@ -13,12 +11,12 @@ module Jekyll
       @text = text
     end
 
-    def render(context)
-      render_tweet()
+    def render(_context)
+      render_tweet
     end
 
-    def render_tweet()
-      if @client == nil
+    def render_tweet
+      if @client.nil?
         @client = Twitter::REST::Client.new do |config|
           config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
           config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
@@ -42,14 +40,13 @@ module Jekyll
         'created_at_display' => tweet.created_at.strftime('%l:%M %p - %b %e, %Y')
       }
 
-      if tweet.favorite_count > 0
+      if tweet.favorite_count.positive?
         params['favorite_count'] = tweet.favorite_count
       end
 
       @template.render(params)
     end
-
   end
 end
 
-Liquid::Template.register_tag("tweet", Jekyll::ServerRenderedTweet)
+Liquid::Template.register_tag('tweet', Jekyll::ServerRenderedTweet)
